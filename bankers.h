790d45d3fff28d5df4b86f261fdbb48929b8fd45
T-Ginger-J <trent.jordan@ontariotechu.net>
2024-03-22 17:58:21 -0400

    initial files

---
+++ bankers.h
@@ -0,0 +1,9 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+//return 0 if successful or -1 if failed
+int requestR(int customerN, int request[]);
+int releaseR(int customerN, int release[]);
